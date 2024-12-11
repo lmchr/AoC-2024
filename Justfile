d72242683105fa63fee1a5b4e775fc7c0d64235d
lmchr <lars.melcher94@gmail.com>
2024-12-11 12:50:06 +0100

    Add profiling

--- Justfile
+++ Justfile
@@ -1,4 +1,15 @@
+profOutput := ".profile.prof"
+
+default:
+  just --list
+
 format:
   go fmt ./...
 lint:
   docker run -t --rm -v $(pwd):/app -w /app golangci/golangci-lint:v1.61.0 golangci-lint run -v
+build:
+  go build
+profile day: build
+  ./aoc-2024 -day {{day}} -cpuprofile {{ profOutput }}
+viewProfile:
+  go tool pprof -http 127.0.0.1:8080 aoc-2024 {{ profOutput }}
--- main.go
+++ main.go
@@ -5,16 +5,33 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
+	"runtime/pprof"
 	"time"
 )
 
 func main() {
-	var intFlag int
-	flag.IntVar(&intFlag, "day", -1, "Provide the day to execute [1..24]")
+	var day int
+	var cpuProfile string
+	flag.IntVar(&day, "day", -1, "Provide the day to execute [1..24]")
+	flag.StringVar(&cpuProfile, "cpuprofile", "", "write cpu profile to file")
 	flag.Parse()
+
+	if cpuProfile != "" {
+		f, err := os.Create(cpuProfile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = pprof.StartCPUProfile(f)
+		if err != nil {
+			panic(err)
+		}
+		defer pprof.StopCPUProfile()
+	}
+
 	log.Printf("AoC-2024")
 	start := time.Now()
-	switch intFlag {
+	switch day {
 	case 1:
 		Days.Day1()
 	case 2:
@@ -30,7 +47,7 @@ func main() {
 	case 7:
 		Days.Day7()
 	default:
-		panic(fmt.Sprintf("Invalid day or not implemented yet: %d. Execute with e.g. '-day 1'", intFlag))
+		panic(fmt.Sprintf("Invalid day or not implemented yet: %d. Execute with e.g. '-day 1'", day))
 	}
 	elapsed := time.Since(start)
 	log.Printf("Execution took %s", elapsed)
